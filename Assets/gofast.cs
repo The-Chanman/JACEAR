3baafb49fd9366c6ee1797db7d58f711a73641c8
AbsoluteAnarchy <45885925+AbsoluteAnarchy@users.noreply.github.com>
2019-02-09 14:20:08 -0500

    added raycaster to JoshScene
    nooope

---
+++ Assets/gofast.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(0, 0, Time.deltaTime);
+
+        // Move the object upward in world space 1 unit/second.
+        transform.Translate(0, Time.deltaTime, 0, Space.World);
+    }
+}
