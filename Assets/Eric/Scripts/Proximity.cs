898dc44c22a94aecbd064aa4865f06dd9ab79769
the-chanman <ecphylo@gmail.com>
2019-02-09 16:53:43 -0500

    Created Proximity Script
    play audio once thing enters a certain distance

---
+++ Assets/Eric/Scripts/Proximity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+ 
+public class Proximity : MonoBehaviour {
+ 
+    public GameObject player;  
+    public bool PlayedAudioOnEnter = false;
+    private Vector3 currentPos;
+    private Vector3 playerDistance;
+    public AudioSource audioPlayer;
+
+    void Start () {
+        currentPos = transform.position;
+    }
+     
+    void Update () {
+        currentPos = transform.position;
+        Vector3 v = currentPos;
+        playerDistance = player.transform.position - v;
+        float magnitude = playerDistance.magnitude;
+        Debug.Log(magnitude);
+        if(magnitude <= 10f){
+            if(!PlayedAudioOnEnter){
+                audioPlayer.Play();
+                PlayedAudioOnEnter = true;
+            }
+        }
+        if(magnitude > 10f){
+            PlayedAudioOnEnter = false;
+        }
+    }
+}
